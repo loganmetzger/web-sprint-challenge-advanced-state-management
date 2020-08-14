f16da80d2534c359fbc0ec419d64f122238eaed5
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-08-14 11:14:11 -0400

    imports to index and created store, wrapped app in provider

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { Provider } from "react-redux"
+import { createStore, applyMiddleware } from "redux"
+import thunk from "redux-thunk"
+import logger from "redux-logger"
+import { smurfReducer } from "./store/reducers/smurfReducer";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(smurfReducer, applyMiddleware(thunk, logger))
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>
+, document.getElementById("root"));

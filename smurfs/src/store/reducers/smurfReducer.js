fc361d46fe746afde3bb836f690df5b28588d8d6
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-08-14 11:09:53 -0400

    created store and reducer folder, declared intitial state and created default switch case

---
+++ smurfs/src/store/reducers/smurfReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    smurfs: [],
+    loading: false,
+    error: ''
+}
+
+export const smurfReducer = (state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
